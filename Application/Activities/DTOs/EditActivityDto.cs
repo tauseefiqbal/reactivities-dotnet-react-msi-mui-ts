5fab345965b1c3f57e4492ab06c85fef22995ccc
tauseefiqbal <tauseef.iqbal@live.com>
2026-03-12 13:54:50 -0600

    Delete Application directory

--- Application/Activities/DTOs/EditActivityDto.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Application.Activities.DTOs;
-
-public class EditActivityDto : BaseActivityDto
-{
-    public string Id { get; set; } = "";
-}
--- Application/Profiles/DTOs/PhotoUploadResult.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Application.Profiles.DTOs;
-
-public class PhotoUploadResult
-{
-    public required string PublicId { get; set; }
-    public required string Url { get; set; }
-}
